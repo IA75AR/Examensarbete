704181480e12b4b31bcce65dd09cb9b149ed5ee5
ingelaarrbrant <ingela.arrbrant@utb.ecutbildning.se>
2023-05-06 15:09:38 +0200

    Start HomePage

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { View, Image, StyleSheet, Text } from 'react-native';
+import KICKSTART from "../assets/KICKSTART.png"
+import LoggaTHRosa from "../assets/LoggaTHVit.png"
 
 export default function Home () {
 
@@ -7,9 +9,29 @@ export default function Home () {
   
     return (
         <>
-        <View>
-            <StatusBar style="dark" />
-        </View>
-     </>
+            <View style={styles.container}>
+                <Image source={LoggaTHRosa} style={styles.imageTH} />
+                <Text>_____________________________________</Text>
+                <Image source={KICKSTART} style={styles.imageKICKSTART} />
+                <StatusBar style="dark" />
+            </View>
+        </>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#ec008c",
+        paddingTop: 50
+    }, imageTH: {
+        height: 150,
+        width: 250
+    }, imageKICKSTART: {
+        height: 200,
+        width: 350
+    }
+});
+
