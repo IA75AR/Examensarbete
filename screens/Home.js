df3d613fb3f6372ed912a3c9b06a0d40a0a10cac
ingelaarrbrant <ingela.arrbrant@utb.ecutbildning.se>
2023-05-17 17:49:32 +0200

    Animated Logos HomeScreen

--- screens/Home.js
+++ screens/Home.js
@@ -1,16 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { View, Image, StyleSheet, Text, Pressable } from 'react-native';
+import { View, StyleSheet, Text, Pressable, Animated } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 import KICKSTART from "../assets/KICKSTART/KICKSTART.png";
 import LoggaTHRosa from "../assets/theHouse/LoggaTHVit.png";
 import MenyScreen from './MenyScreen';
 import Fitness from './Fitness';
+import { useRef, useEffect } from 'react';
 
 export default function Home () {
 
     //"Home"-screen
 
     const navigation = useNavigation();
+    const animation = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(animation, {
+                    toValue: 1,
+                    duration: 2000,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(animation, {
+                    toValue: 0,
+                    duration: 2000,
+                    useNativeDriver: true,
+                }),
+            ]),
+            { iterations: 100 }
+        ).start();
+    }, [animation]);
+
 
     const handleChangeScreenTH = () => {
         navigation.navigate("MenyScreen", { screen: {MenyScreen}})
@@ -24,11 +45,11 @@ export default function Home () {
         <>
             <View style={styles.container}>
                 <Pressable onPress={handleChangeScreenTH}>
-                    <Image source={LoggaTHRosa} style={styles.imageTH}/>
+                    <Animated.Image source={LoggaTHRosa} style={[styles.imageTH, { opacity: animation}]}/>
                 </Pressable>
                 <Text>_____________________________________</Text>
                 <Pressable onPress={handleChangeScreenKI}>
-                    <Image source={KICKSTART} style={styles.imageKICKSTART}/>
+                    <Animated.Image source={KICKSTART} style={[styles.imageKICKSTART, { opacity: animation}]}/>
                 </Pressable>
                 <Text style={styles.version}>Version: 10.0.20</Text>
                 <StatusBar style="dark" />
